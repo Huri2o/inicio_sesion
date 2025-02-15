c6a230a2a2eac01d781bb9cf6694fc465671d8ca
Uriel_C <uriel_ci@si.com>
2025-02-14 19:39:57 -0600

    version mac1

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "0505Leon";
+$password = "12345678";
 $dbname = "prueba";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
